333f112d4bece1c04db721f5402b984d950c586d
takumu <s186662v@st.go.tuat.ac.jp>
2021-03-18 23:36:26 +0900

    first commit

---
+++ tokenize.py
@@ -0,0 +1,51 @@
+from pprint import pprint as pp
+# coding:utf-8
+import MeCab
+import csv
+
+with open('companies.csv') as f:
+    reader = csv.reader(f)
+    data = [x for x in reader]
+
+sentence = "文書"
+sentence = data[1][4]
+print(sentence)
+# MeCabの形態素解析器(Tagger)の初期化
+tagger = MeCab.Tagger()
+tagger.parse('')
+
+# sentenceをTaggerによって形態素解析する
+# 結果はNodeクラスのオブジェクトの単方向リストのルートノードとして返ってくる．
+node = tagger.parseToNode(sentence)
+
+print("=" * 50)
+while node:
+	# 形態素の表層形
+	print(node.surface)
+
+	# node.feature にはそれぞれの形態素の詳細がカンマ区切りで含まれている
+	features = node.feature.split(",")
+	print(features)
+	features = [features[i] if len(features) > i else None for i in range(0, 9)]
+
+	# features[0]: 品詞
+	print("\t品詞\t: " + features[0])
+	# features[1]: 品詞細分類1
+	print("\t品詞細分類1\t: " + features[1])
+	# features[2]: 品詞細分類2
+	print("\t品詞細分類2\t: " + features[2])
+	# features[3]: 品詞細分類3
+	print("\t品詞細分類3\t: " + features[3])
+	# features[4]: 活用形
+	print("\t活用形\t: " + features[4])
+	# features[5]: 活用型
+	print("\t活用型\t: " + features[5])
+	# features[6]: 原形
+	print("\t原形\t: " + features[6])
+	# features[7]: 読み
+	print("\t読み\t: " + features[7])
+	# features[8]: 発音
+	print("\t発音\t: " + features[8])
+	print("=" * 50)
+	node = node.next
+	pass
